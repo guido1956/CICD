8b71ef149a7dbbdb05f654245fc83a56e1669ff2
Guido1956 <GuidoDulos@valori.nl>
2021-04-20 11:17:29 +0200

    Calculator aftrekfunctie toegevoegd

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,4 +2,8 @@ public class Calculator {
     public int vermenigvuldig(int getal1 , int getal2) {
         return getal1 * getal2;
     }
+
+    public int aftrekken(int getal1, int getal2) {
+        return getal1 -getal2;
+    }
 }
