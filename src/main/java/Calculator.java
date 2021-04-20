c3781f3ab20605b6cdeef8ae84d228d042f2d33d
Guido <GuidoDulos@valori.nl>
2021-04-20 11:15:26 +0200

    Versie2 optellen

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,4 +2,5 @@ public class Calculator {
     public int vermenigvuldig(int getal1 , int getal2) {
         return getal1 * getal2;
     }
+    public int optellen(int getal1, int getal2) {return getal1 + getal2;}
 }
