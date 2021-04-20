886cbe792c515f7a90d4bc480ea8e4f5f274aa29
Guido1956 <GuidoDulos@valori.nl>
2021-04-20 09:55:59 +0200

    Calculator versie1

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,5 @@
+public class Calculator {
+    public int vermenigvuldig(int getal1 , int getal2) {
+        return getal1 * getal2;
+    }
+}
---
+++ src/main/java/Rekenen.java
@@ -0,0 +1,6 @@
+public class Rekenen {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.vermenigvuldig(3, 5));
+    }
+}
